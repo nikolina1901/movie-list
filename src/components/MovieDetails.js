75319a44daa9a4b908e8391f6bce7036c03c51a6
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-04-21 11:03:35 +0100

    finished router/params challenge

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,9 +1,43 @@
+import axios from "axios";
 import React from "react";
+import { Link } from "react-router-dom";
+import moviesData from "./Data";
+import Movie from "./Movie";
+import MovieList from "./MovieList";
 
-function MovieDetails() {
+function MovieDetails(props) {
+  const id = props.match.params.id;
+  const [movies, setMovies] = React.useState(moviesData);
+  React.useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = () => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/wildcodeschoolparis/datas/master/movies.json"
+      )
+      .then(function (response) {
+        // handle success
+        setMovies(response.data.movies);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+  const filterMoviesId = movies
+    .filter((movie) => {
+      return movie.id == id;
+    })
+    .map((movie) => {
+      return <Movie key={movie.id} {...movie} />;
+    });
   return (
     <div>
-      <h1>Movie Details</h1>
+      <h2>{filterMoviesId}</h2>
+
+      <Link to="/movielist">Go back to the list</Link>
     </div>
   );
 }
